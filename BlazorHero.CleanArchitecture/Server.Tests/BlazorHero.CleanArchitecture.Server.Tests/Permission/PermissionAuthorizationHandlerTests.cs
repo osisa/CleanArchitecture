1c45144007304af5227d99ac5c776174800df506
OserRemo <remo.oser@osisa.com>
2021-06-17 09:43:36 +0200

    Internals visible first compile

---
+++ BlazorHero.CleanArchitecture/Server.Tests/BlazorHero.CleanArchitecture.Server.Tests/Permission/PermissionAuthorizationHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using BlazorHero.CleanArchitecture.Server.Permission;
+
+using FluentAssertions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlazorHero.CleanArchitecture.Server.Tests.Permission
+{
+    [TestClass]
+    public class PermissionAuthorizationHandlerTests
+    {
+        #region Public Methods and Operators
+
+        [TestMethod]
+        public void Constructor()
+        {
+            // Arrange
+
+            // Act
+            var result = CreateUnitUnderTest();
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static PermissionAuthorizationHandler CreateUnitUnderTest()
+        {
+            return new PermissionAuthorizationHandler();
+        }
+
+        #endregion
+    }
+}
